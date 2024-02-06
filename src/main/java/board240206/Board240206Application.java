ee32d13113dc02f58107f69618a6c2ebfc02d3ba
jserin <jsr3643@naver.com>
2024-02-06 14:42:04 +0900

    first commit

---
+++ src/main/java/board240206/Board240206Application.java
@@ -0,0 +1,13 @@
+package board240206;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Board240206Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Board240206Application.class, args);
+	}
+
+}
---
+++ src/test/java/board240206/board240206/Board240206ApplicationTests.java
@@ -0,0 +1,13 @@
+package board240206.board240206;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Board240206ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
